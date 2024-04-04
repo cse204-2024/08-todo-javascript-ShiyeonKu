2e606b92fb20c3b289a2f158a06850abd249fa10
Shiyeon Ku <c.monk@wustl.edu>
2024-04-04 00:07:52 -0500

    updated visual
    updated visual system with now focus on creating functions

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+//grab todo list from the api
+initialize();
 
+function initialize(){
+    let xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function(){
+        if (this.readyState == 4 && this.status == 200){
+            let todos = JSON.parse(this.responseText);
+            console.log(todos);
+        }
+    };
+    
+    xhttp.open("GET", "https://cse204.work/todos", true);
+    xhttp.setRequestHeader("x-api-key", "d4681d-747376-1752ce-4282a1-053f50");
+    xhttp.send();
+}
+
+document.getElementById("submit_todo").addEventListener("click", postTodo());
+
+//set variable from the form as a new todo and add to the list
+
+function postTodo(){
+    console.log(document.getElementById("add_todo").value);
+}
