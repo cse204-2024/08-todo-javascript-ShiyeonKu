fe599014387cfd1fa5f3ef8c355ead70787cae32
Shiyeon Ku <c.monk@wustl.edu>
2024-04-04 01:20:23 -0500

    new changes to js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //grab todo list from the api
+initialize();
 document.getElementById("submit_todo").addEventListener("click", postTodo);
 
 function initialize(){
@@ -6,18 +7,48 @@ function initialize(){
 
     xhttp.onreadystatechange = function(){
         if (this.readyState == 4 && this.status == 200){
-            let todos = JSON.parse(this.responseText);
-            console.log(todos);
+            let todo = JSON.parse(this.responseText);
+            console.log(todo);
         }
     };
     
     xhttp.open("GET", "https://cse204.work/todos", true);
     xhttp.setRequestHeader("x-api-key", "d4681d-747376-1752ce-4282a1-053f50");
     xhttp.send();
+    
 }
-
 
 //set variable from the form as a new todo and add to the list
 function postTodo(){
-    console.log(document.getElementById("add_todo").value);
+    let data={
+        text: document.getElementById("add_todo").value
+    };
+    console.log(JSON.stringify(data));
+
+    let xhttp2 = new XMLHttpRequest();
+
+    xhttp2.onreadystatechange = function(){
+        if (this.readyState == 4 && this.status == 200){
+            let todo = JSON.parse(this.responseText);
+            console.log(todo);
+            for (i = 0; i < todo.length; i++){
+                let newtodo = document.createElement("input");
+                newtodo.setAttribute("type", "checkbox");
+                newtodo.setAttribute("value", todo[i].text);
+                document.getElementById("nc_todo").appendChild(newtodo);
+            }
+        }else if(this.readyState == 4){
+            console.log(this.responseText);
+        }
+    };
+
+    xhttp2.open("POST", "https://cse204.work/todos", true);
+
+    xhttp2.setRequestHeader("Content-type", "application/json");
+    xhttp2.setRequestHeader("x-api-key", "d4681d-747376-1752ce-4282a1-053f50");
+    xhttp2.send(JSON.stringify(data));
+}
+
+function getTodo(){
+
 }
